6173cd87c64b026ff3a3364aa2eea2a9bee36919
May <maymchen1@gmail.com>
2023-06-19 17:48:44 -0400

    m

--- my-app/app/app.js
+++ my-app/app/app.js
@@ -2,3 +2,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import axios from 'axios';
+
+import Home from './components/Home';
+import UserProfile from './components/UserProfile';
+import GameDetails from './components/GameDetails';
