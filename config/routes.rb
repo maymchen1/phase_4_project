e9b4afdfb7c91c135be20ca783125300bb3827a1
May <maymchen1@gmail.com>
2023-06-19 21:06:10 -0400

    m

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ # config/routes.rb
+ Rails.application.routes.draw do
+  namespace :api do
+    resources :users, only: [:create]
+  end
 end
